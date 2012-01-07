b652fec8821ede31a687581bf14a7d79397a5a9a
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-01-07 10:07:08 -0600

    defines a namespace and the members class so rspec runs.

---
+++ lib/trello.rb
@@ -0,0 +1,7 @@
+# Ruby wrapper around the Trello API
+# Copyright (c) 2012, Jeremy Tregunna
+# Use and distribution terms may be found in the file LICENSE included in this distribution.
+
+module Trello
+  autoload :Member, 'trello/member'
+end
---
+++ lib/trello/member.rb
@@ -0,0 +1,8 @@
+# Ruby wrapper around the Trello API
+# Copyright (c) 2012, Jeremy Tregunna
+# Use and distribution terms may be found in the file LICENSE included in this distribution.
+
+module Trello
+  class Member
+  end
+end
