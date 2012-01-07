3feee7e51e39278c9d3c3b3f934528e07e4e7a7d
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-01-07 10:06:34 -0600

    a bunch of pending member specs!

---
+++ spec/member_spec.rb
@@ -0,0 +1,31 @@
+# Specs covering the members namespace in the Trello API
+
+require 'spec_helper'
+
+describe Trello::Member do
+  context "actions" do
+    # This spec needs a better name
+    it "retrieves a list of actions"
+  end
+
+  context "boards" do
+  end
+
+  context "cards" do
+  end
+
+  context "organizations" do
+  end
+
+  context "personal" do
+    it "gets the members bio"
+
+    it "gets the full name"
+
+    it "gets the gravatar id"
+
+    it "gets the url"
+
+    it "gets the username"
+  end
+end
