9e45777b1121d66b27b6d410846a6a1fa9361925
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-01-10 12:19:41 -0600

    fixes stub for cases with no parameters, and adds an additional parameter to indicate what value the call returns

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,13 +17,13 @@ require 'trello'
 require 'webmock/rspec'
 
 module Helpers
-  def stub_trello_request!(http_method, path, data)
+  def stub_trello_request!(http_method, path, data, returning = '')
     uri = Addressable::URI.parse("https://api.trello.com/#{Trello::API_VERSION}#{path}")
-    uri.query_values = data.kind_of?(String) ? JSON.parse(data) : data
+    uri.query_values = data.kind_of?(String) ? JSON.parse(data) : data if data
 
     stub_request(http_method, uri.to_s).
       with(:headers => {'Accept'=>'*/*', 'Authorization'=>/.*/, 'User-Agent' => /.*/}).
-      to_return(:status => 200, :headers => {}, :body => '')
+      to_return(:status => 200, :headers => {}, :body => returning)
   end
 
   def user_details
