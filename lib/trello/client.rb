4d5290583de4f0e509ed007d8eb5441519162f69
Ben Biddington <ben.biddington@gmail.com>
2012-01-20 22:13:29 +1300

    Cleared out unused classes and methods

--- lib/trello/client.rb
+++ lib/trello/client.rb
@@ -2,9 +2,6 @@ require 'addressable/uri'
 
 module Trello
   class Client
-    class EnterYourPublicKey < StandardError; end
-    class EnterYourSecret < StandardError; end
-
     class << self
       def get(path, params = {})
         api_version = 1
@@ -14,19 +11,6 @@ module Trello
         request = Request.new uri, {}
 
         response = TInternet.get AuthPolicy.authorize(request)
-
-        raise Error, response.message if response.code.to_i != 200
-
-        response
-      end
-
-      private
-
-      def query(api_version, path, options = { :method => :get, :params => {} })
-        uri = Addressable::URI.parse("https://api.trello.com/#{api_version}#{path}")
-        uri.query_values = options[:params]
-
-        response = TInternet.send(options[:method], uri, {}, nil)
 
         raise Error, response.message if response.code.to_i != 200
 
