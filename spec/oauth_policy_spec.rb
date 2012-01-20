42c1c3292d7c07943498503937c71809cd12693e
Ben Biddington <ben.biddington@gmail.com>
2012-01-20 21:11:45 +1300

    OAuthPolicy preserves query parameters

--- spec/oauth_policy_spec.rb
+++ spec/oauth_policy_spec.rb
@@ -21,6 +21,14 @@ describe OAuthPolicy do
     
     authorized_request.headers.keys.should include "Authorization"
   end
+  
+  it "preserves query parameters" do
+    uri = Addressable::URI.parse("https://xxx/?name=Riccardo")
+    request = Request.new uri
 
-  it "preserves query parameters"
+    authorized_request = OAuthPolicy.authorize request
+    
+    the_query_parameters = Addressable::URI.parse(authorized_request.uri).query_values
+    the_query_parameters.should == {"name" => "Riccardo"}
+  end
 end
