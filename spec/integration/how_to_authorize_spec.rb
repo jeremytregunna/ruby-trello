f0742550679d809bbbe7aa404aaea0cb25340395
Ben Biddington <ben.biddington@gmail.com>
2012-01-27 11:55:30 +0800

    Refactor: moved IntegrationTest to its own file

--- spec/integration/how_to_authorize_spec.rb
+++ spec/integration/how_to_authorize_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'integration/integration_test'
 
 include Trello
 include Trello::Authorization
@@ -9,35 +10,6 @@ class Container
       parent.send :remove_const, name
       parent.const_set name, value
     end
-  end
-end
-
-module IntegrationTest
-  def self.included(clazz)
-    clazz.class_eval do
-      before :all do
-        # Getting developer/member key
-        # 1. https://trello.com/1/appKey/generate
-        # 2. https://trello.com/1/connect?key=<public_key_here>&name=RubyTrelloIntegrationTests&response_type=token
-        # See: https://trello.com/board/trello-public-api/4ed7e27fe6abb2517a21383d
-
-        @developer_public_key = ENV["DEVELOPER_PUBLIC_KEY"]
-        @developer_secret     = ENV["DEVELOPER_SECRET"]
-        @member_token         = ENV["MEMBER_TOKEN"]
-        @welcome_board        = ENV["WELCOME_BOARD"]
-        @access_token_key     = ENV["ACCESS_TOKEN_KEY"]
-        @access_token_secret  = ENV["ACCESS_TOKEN_SECRET"]
-        
-        WebMock.disable!
-      end    
-    end
-  end
-
-  protected
-
-  def get(uri)
-    require "rest_client"
-    RestClient.get uri.to_s
   end
 end
 
