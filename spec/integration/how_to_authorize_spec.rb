4f50b5f8aab737bd6ee429dd582f6a7c51bc7c60
Ben Biddington <ben.biddington@gmail.com>
2012-01-30 07:21:34 +0800

    Refactor: moved the board tests to their own file

--- spec/integration/how_to_authorize_spec.rb
+++ spec/integration/how_to_authorize_spec.rb
@@ -46,35 +46,3 @@ describe "Authorizing read-only requests" do
     welcome_board.id.should === @welcome_board
   end
 end
-
-describe "Authorizing read/write requests" do
-  include IntegrationTest
-
-  context "given a valid access token" do
-    before :all do
-      OAuthPolicy.consumer_credential = OAuthCredential.new @developer_public_key, @developer_secret
-      OAuthPolicy.token = OAuthCredential.new @access_token_key, @access_token_secret
-      Container.set Trello::Authorization, "AuthPolicy", OAuthPolicy
-    end
-
-    it "can add a board" do
-      new_board = Board.create(:name => "An example")
-      new_board.should_not be_nil
-      new_board.id.should_not be_nil
-      new_board.name.should == "An example"
-      new_board.should_not be_closed
-    end
-
-    it "can read the welcome board" do
-      welcome_board = Board.find @welcome_board
-      welcome_board.name.should === "Welcome Board"
-      welcome_board.id.should === @welcome_board
-    end
-
-    it "can close a board" do
-      new_board = Board.create(:name => "An example")
-
-      Client.put "/boards/#{new_board.id}/closed", { :value => true }
-    end
-  end
-end
---
+++ spec/integration/how_to_use_boards_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+require 'integration/integration_test'
+
+include Trello
+include Trello::Authorization
+
+class Container
+  class << self
+    def set(parent, name, value)
+      parent.send :remove_const, name
+      parent.const_set name, value
+    end
+  end
+end
+
+describe "Authorizing read/write requests" do
+  include IntegrationTest
+
+  context "given a valid access token" do
+    before :all do
+      OAuthPolicy.consumer_credential = OAuthCredential.new @developer_public_key, @developer_secret
+      OAuthPolicy.token = OAuthCredential.new @access_token_key, @access_token_secret
+      Container.set Trello::Authorization, "AuthPolicy", OAuthPolicy
+    end
+
+    it "can add a board" do
+      new_board = Board.create(:name => "An example")
+      new_board.should_not be_nil
+      new_board.id.should_not be_nil
+      new_board.name.should == "An example"
+      new_board.should_not be_closed
+    end
+
+    it "can read the welcome board" do
+      welcome_board = Board.find @welcome_board
+      welcome_board.name.should === "Welcome Board"
+      welcome_board.id.should === @welcome_board
+    end
+
+    it "can close a board" do
+      new_board = Board.create(:name => "An example")
+
+      Client.put "/boards/#{new_board.id}/closed", { :value => true }
+    end
+  end
+end
