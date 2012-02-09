cc8126a207fbfd0a1fe953468e373938e5992588
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-02-09 13:41:01 -0600

    removes caching from action

--- lib/trello/action.rb
+++ lib/trello/action.rb
@@ -24,26 +24,22 @@ module Trello
 
     # Returns the board this action occurred on.
     def board
-      return @board if @board
-      @board = Client.get("/actions/#{id}/board").json_into(Board)
+      Client.get("/actions/#{id}/board").json_into(Board)
     end
 
     # Returns the card the action occurred on.
     def card
-      return @card if @card
-      @card = Client.get("/actions/#{id}/card").json_into(Card)
+      Client.get("/actions/#{id}/card").json_into(Card)
     end
 
     # Returns the list the action occurred on.
     def list
-      return @list if @list
-      @list = Client.get("/actions/#{id}/list").json_into(List)
+      Client.get("/actions/#{id}/list").json_into(List)
     end
 
     # Returns the member who created the action.
     def member_creator
-      return @member_creator if @member_creator
-      @member_creator = Member.find(member_creator_id)
+      Member.find(member_creator_id)
     end
   end
 end
