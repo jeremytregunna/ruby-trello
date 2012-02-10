1f1b68e16de52ed9a930073a785e99e0214ef329
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-02-10 00:28:27 -0600

    removes errant ,

--- lib/trello/basic_data.rb
+++ lib/trello/basic_data.rb
@@ -15,7 +15,7 @@ module Trello
     def self.register_attributes(*names)
       # Defines the attribute getter and setters.
       class_eval do
-        define_method :attributes, do
+        define_method :attributes do
           @attributes ||= names.inject({}) { |hash,k| hash.merge(k.to_sym => nil) }
         end
 
