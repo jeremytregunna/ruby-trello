bbab77418d12ad71f4989e727f6d6d67a36e0939
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-02-10 00:03:58 -0600

    converts item to activemodel

--- lib/trello/item.rb
+++ lib/trello/item.rb
@@ -1,16 +1,17 @@
 module Trello
   # An Item is a basic task that can be checked off and marked as completed.
   class Item < BasicData
-    attr_reader :id, :name, :type
+    register_attributes :id, :name, :type
+    validates_presence_of :id, :type
 
     # Updates the fields of an item.
     #
     # Supply a hash of string keyed data retrieved from the Trello API representing
     # an item.
     def update_fields(fields)
-      @id   = fields['id']
-      @name = fields['name']
-      @type = fields['type']
+      attributes[:id]   = fields['id']
+      attributes[:name] = fields['name']
+      attributes[:type] = fields['type']
       self
     end
   end
