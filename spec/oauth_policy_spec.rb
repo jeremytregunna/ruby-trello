fd704f85c5d61fe96a40697e963fe13db0252966
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-02-12 13:24:19 -0600

    fixes oauth tests

--- spec/oauth_policy_spec.rb
+++ spec/oauth_policy_spec.rb
@@ -9,74 +9,85 @@ describe OAuthPolicy do
     OAuthPolicy.token = nil
   end
 
-  it "adds an authorization header" do 
-    uri = Addressable::URI.parse("https://xxx/")
+  context "2-legged" do
+    it "adds an authorization header" do 
+      uri = Addressable::URI.parse("https://xxx/")
 
-    request = Request.new :get, uri
+      request = Request.new :get, uri
 
-    authorized_request = OAuthPolicy.authorize request
+      OAuthPolicy.token               = OAuthCredential.new "token", nil
+
+      authorized_request = OAuthPolicy.authorize request
+      
+      authorized_request.headers.keys.should include "Authorization"
+    end
     
-    authorized_request.headers.keys.should include "Authorization"
+    it "preserves query parameters" do
+      uri = Addressable::URI.parse("https://xxx/?name=Riccardo")
+      request = Request.new :get, uri
+
+      Clock.stub(:timestamp).and_return "1327048592"
+      Nonce.stub(:next).and_return "b94ff2bf7f0a5e87a326064ae1dbb18f"
+      OAuthPolicy.consumer_credential = OAuthCredential.new "consumer_key", "consumer_secret"
+      OAuthPolicy.token               = OAuthCredential.new "token", nil
+
+      authorized_request = OAuthPolicy.authorize request
+      
+      the_query_parameters = Addressable::URI.parse(authorized_request.uri).query_values
+      the_query_parameters.should == {"name" => "Riccardo"}
+    end
+
+    it "adds the correct signature as part of authorization header" do 
+      Clock.stub(:timestamp).and_return "1327048592"
+      Nonce.stub(:next).and_return "b94ff2bf7f0a5e87a326064ae1dbb18f"
+
+      OAuthPolicy.consumer_credential = OAuthCredential.new "consumer_key", "consumer_secret"
+      OAuthPolicy.token               = OAuthCredential.new "token", nil
+
+      request = Request.new :get, Addressable::URI.parse("http://xxx/")
+
+      authorized_request = OAuthPolicy.authorize request
+      
+      authorized_request.headers["Authorization"].should =~ /oauth_signature="kLcSxrCTd4ATHcLmTp8q%2Foa%2BFMA%3D"/
+    end
+
+    it "adds correct signature for uri with parameters" do
+      Clock.stub(:timestamp).and_return "1327351010"
+      Nonce.stub(:next).and_return "f5474aaf44ca84df0b09870044f91c69"
+
+      OAuthPolicy.consumer_credential = OAuthCredential.new "consumer_key", "consumer_secret"
+      OAuthPolicy.token               = OAuthCredential.new "token", nil
+
+      request = Request.new :get, Addressable::URI.parse("http://xxx/?a=b")
+
+      authorized_request = OAuthPolicy.authorize request
+      
+      authorized_request.headers["Authorization"].should =~ /oauth_signature="xm%2FJ1swxxPb6mnuR1Q1ucJMdGRk%3D"/
+    end
+
+    it "fails if consumer_credential is unset" do
+      OAuthPolicy.consumer_credential = nil
+
+      request = Request.new :get, Addressable::URI.parse("http://xxx/")
+
+      lambda{OAuthPolicy.authorize request}.should raise_error "The consumer_credential has not been supplied."
+    end
+
+    it "can sign with token" do
+      Clock.stub(:timestamp).and_return "1327360530"
+      Nonce.stub(:next).and_return "4f610cb28e7aa8711558de5234af1f0e"
+
+      OAuthPolicy.consumer_credential = OAuthCredential.new "consumer_key", "consumer_secret"
+      OAuthPolicy.token  = OAuthCredential.new "token_key", "token_secret"
+
+      request = Request.new :get, Addressable::URI.parse("http://xxx/")
+
+      authorized_request = OAuthPolicy.authorize request
+
+      authorized_request.headers["Authorization"].should =~ /oauth_signature="3JeZSzsLCYnGNdVALZMgbzQKN44%3D"/
+    end
+
+    it "adds correct signature for https uri"
+    it "adds correct signature for verbs other than get"
   end
-  
-  it "preserves query parameters" do
-    uri = Addressable::URI.parse("https://xxx/?name=Riccardo")
-    request = Request.new :get, uri
-
-    authorized_request = OAuthPolicy.authorize request
-    
-    the_query_parameters = Addressable::URI.parse(authorized_request.uri).query_values
-    the_query_parameters.should == {"name" => "Riccardo"}
-  end
-
-  it "adds the correct signature as part of authorization header" do 
-    Clock.stub(:timestamp).and_return "1327048592"
-    Nonce.stub(:next).and_return "b94ff2bf7f0a5e87a326064ae1dbb18f"
-
-    OAuthPolicy.consumer_credential = OAuthCredential.new "consumer_key", "consumer_secret"
-
-    request = Request.new :get, Addressable::URI.parse("http://xxx/")
-
-    authorized_request = OAuthPolicy.authorize request
-    
-    authorized_request.headers["Authorization"].should =~ /oauth_signature="u7CmId4WEDUqPdHnWVf1JVChFmg%3D"/
-  end
-
-  it "adds correct signature for uri with parameters" do
-    Clock.stub(:timestamp).and_return "1327351010"
-    Nonce.stub(:next).and_return "f5474aaf44ca84df0b09870044f91c69"
-
-    OAuthPolicy.consumer_credential = OAuthCredential.new "consumer_key", "consumer_secret"
-
-    request = Request.new :get, Addressable::URI.parse("http://xxx/?a=b")
-
-    authorized_request = OAuthPolicy.authorize request
-    
-    authorized_request.headers["Authorization"].should =~ /oauth_signature="ABL%2FcOSGJSbvvLt1gW2nV9i%2FDyA%3D"/
-  end
-
-  it "fails if consumer_credential is unset" do
-    OAuthPolicy.consumer_credential = nil
-
-    request = Request.new :get, Addressable::URI.parse("http://xxx/")
-
-    lambda{OAuthPolicy.authorize request}.should raise_error "The consumer_credential has not been supplied."
-  end
-
-  it "can sign with token" do
-    Clock.stub(:timestamp).and_return "1327360530"
-    Nonce.stub(:next).and_return "4f610cb28e7aa8711558de5234af1f0e"
-
-    OAuthPolicy.consumer_credential = OAuthCredential.new "consumer_key", "consumer_secret"
-    OAuthPolicy.token  = OAuthCredential.new "token_key", "token_secret"
-
-    request = Request.new :get, Addressable::URI.parse("http://xxx/")
-
-    authorized_request = OAuthPolicy.authorize request
-
-    authorized_request.headers["Authorization"].should =~ /oauth_signature="1Boj4fo6KiXA4xGD%2BKF5QOD36PI%3D"/
-  end
-
-  it "adds correct signature for https uri"
-  it "adds correct signature for verbs other than get"
 end
