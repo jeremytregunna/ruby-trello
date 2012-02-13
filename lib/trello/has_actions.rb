583f61e7edf36efe7cfbc393895621bd57c7c82d
Jeremy Tregunna <jeremy.tregunna@me.com>
2012-02-12 19:48:38 -0600

    hasactions gets association wrappers

--- lib/trello/has_actions.rb
+++ lib/trello/has_actions.rb
@@ -2,7 +2,8 @@ module Trello
   module HasActions
     # Returns a list of the actions associated with this object.
     def actions(options = {})
-      Client.get("#{request_prefix}/actions", { :filter => :all }.merge(options)).json_into(Action)
+      actions = Client.get("#{request_prefix}/actions", { :filter => :all }.merge(options)).json_into(Action)
+      MultiAssociation.new(self, actions).proxy
     end
   end
 end
