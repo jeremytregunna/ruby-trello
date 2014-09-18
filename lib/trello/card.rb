2e03fc1cabd139e2bf5781f79af629d4f0ad3495
Eyal Eizenberg <eyal.eizenberg+github@samanage.com>
2014-09-18 17:56:54 +0300

    get card position

--- lib/trello/card.rb
+++ lib/trello/card.rb
@@ -67,7 +67,7 @@ module Trello
       attributes[:board_id]           = fields[SYMBOL_TO_STRING[:board_id]]
       attributes[:member_ids]         = fields[SYMBOL_TO_STRING[:member_ids]]
       attributes[:list_id]            = fields[SYMBOL_TO_STRING[:list_id]]
-      attributes[:pos]                = fields[SYMBOL_TO_STRING[:post]]
+      attributes[:pos]                = fields[SYMBOL_TO_STRING[:pos]]
       attributes[:card_labels]        = fields[SYMBOL_TO_STRING[:card_labels]]
       attributes[:last_activity_date] = Time.iso8601(fields[SYMBOL_TO_STRING[:last_activity_date]]) rescue nil
       attributes[:cover_image_id]     = fields[SYMBOL_TO_STRING[:cover_image_id]]
