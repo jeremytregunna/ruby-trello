7795881cf44acb6f39b7502b3f5a1893d043fa82
Artem Bykov <artem.bikov@gmail.com>
2015-03-11 12:00:58 +0300

    parse all info about labels

--- lib/trello/label.rb
+++ lib/trello/label.rb
@@ -2,7 +2,7 @@ module Trello
 
   # A colored Label attached to a card
   class Label < BasicData
-    register_attributes :name, :color
+    register_attributes :name, :color, :uses, :id
 
     # Update the fields of a label.
     #
@@ -11,14 +11,10 @@ module Trello
     def update_fields(fields)
       attributes[:name]  = fields['name']
       attributes[:color] = fields['color']
+      attributes[:uses] = fields['uses']
+      attributes[:id] = fields['id']
       self
     end
-
-    def cards 
-      cards = client.get("/cards/labels/#{name}").json_into(Card)
-      MultiAssociation.new(self, cards).proxy
-    end
-
   end
 
 end
