8817e0dd86eaec6be9216de28c1b772ebb935aeb
Adam Cuppy <adam@codingzeal.com>
2015-09-03 16:06:40 -0700

    Migrate spec/array_spec.rb to `expect` syntax

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -4,33 +4,44 @@ module Trello
   describe Action do
     include Helpers
 
-    let(:action) { client.find(:action, '4ee2482134a81a757a08af47') }
-    let(:client) { Client.new }
+    let(:client)  { Client.new }
+    let(:action)  { client.find(:action, '4ee2482134a81a757a08af47') }
 
-    before(:each) do
-      client.stub(:get).with('/actions/4ee2482134a81a757a08af47', {}).
-        and_return JSON.generate(actions_details.first)
+    before do
+      allow(client)
+        .to receive(:get)
+        .with('/actions/4ee2482134a81a757a08af47', {})
+        .and_return JSON.generate(actions_details.first)
     end
 
     context 'finding' do
       let(:client) { Trello.client }
 
       it 'delegates to Trello.client#find' do
-        client.should_receive(:find).with(:action, '4ee2482134a81a757a08af47', {})
+        expect(client)
+          .to receive(:find)
+          .with(:action, '4ee2482134a81a757a08af47', {})
+
         Action.find('4ee2482134a81a757a08af47')
       end
 
-      it 'is equivalent to client#find' do
-        Action.find('4ee2482134a81a757a08af47').should eq(action)
+      it do
+        expect(Action.find('4ee2482134a81a757a08af47')).to eq(action)
       end
     end
 
     context 'search' do
       let(:client) { Trello.client }
+      let(:payload) { JSON.generate({ "cards" => cards_details }) }
 
-      it "should search and get back a card object" do
-        client.should_receive(:get).with("/search/", { query: "something"}).and_return(JSON.generate({ "cards" => cards_details }))
-        Action.search("something").should eq({ "cards" => cards_details.jsoned_into(Card) })
+      it "searches and get back a card object" do
+        expect(client)
+          .to receive(:get)
+          .with("/search/", { query: "something"})
+          .and_return payload
+
+        expect(Action.search("something"))
+          .to eq({ "cards" => cards_details.jsoned_into(Card) })
       end
     end
 
@@ -38,54 +49,79 @@ module Trello
       let(:detail) { actions_details.first }
 
       it 'gets its id' do
-        action.id.should == detail['id']
+        expect(action.id).to eq detail['id']
       end
 
       it 'gets its type' do
-        action.type.should == detail['type']
+        expect(action.type).to eq detail['type']
       end
 
       it 'has the same data' do
-        action.data.should == detail['data']
+        expect(action.data).to eq detail['data']
       end
 
       it 'gets the date' do
-        action.date.utc.iso8601.should == detail['date']
+        expect(action.date.utc.iso8601).to eq detail['date']
       end
     end
 
     context 'boards' do
-      it 'has a board' do
-        client.stub(:get).with('/actions/4ee2482134a81a757a08af47/board').
-          and_return JSON.generate(boards_details.first)
+      let(:payload) { JSON.generate(boards_details.first) }
 
-        action.board.should_not be_nil
+      before do
+        allow(client)
+          .to receive(:get)
+          .with('/actions/4ee2482134a81a757a08af47/board')
+          .and_return payload
+      end
+
+      it 'has a board' do
+        expect(action.board).to_not be_nil
       end
     end
 
-    context 'card' do
-      it 'has a card' do
-        client.stub(:get).with('/actions/4ee2482134a81a757a08af47/card').
-          and_return JSON.generate(cards_details.first)
 
-        action.card.should_not be_nil
+    context 'card' do
+      let(:payload) { JSON.generate(cards_details.first) }
+
+      before do
+        allow(client)
+          .to receive(:get)
+          .with('/actions/4ee2482134a81a757a08af47/card')
+          .and_return payload
+      end
+
+      it 'has a card' do
+        expect(action.card).to_not be_nil
       end
     end
 
     context 'list' do
-      it 'has a list of lists' do
-        client.stub(:get).with('/actions/4ee2482134a81a757a08af47/list').
-          and_return JSON.generate(lists_details.first)
+      let(:payload) { JSON.generate(lists_details.first) }
 
-        action.list.should_not be_nil
+      before do
+        allow(client)
+          .to receive(:get)
+          .with('/actions/4ee2482134a81a757a08af47/list')
+          .and_return payload
+      end
+
+      it 'has a list of lists' do
+        expect(action.list).to_not be_nil
       end
     end
 
     context 'member creator' do
-      it 'knows its member creator' do
-        client.stub(:get).with('/members/abcdef123456789123456789', {}).and_return user_payload
 
-        action.member_creator.should_not be_nil
+      before do
+        allow(client)
+          .to receive(:get)
+          .with('/members/abcdef123456789123456789', {})
+          .and_return user_payload
+      end
+
+      it 'knows its member creator' do
+        expect(action.member_creator).to_not be_nil
       end
     end
   end
