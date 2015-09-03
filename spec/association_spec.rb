f3c41e9eb392464090da7e7545b335b3b9e4390e
Adam Cuppy <adam@codingzeal.com>
2015-09-03 16:11:51 -0700

    Migrate spec/association_spec.rb to `expect` syntax

--- spec/association_spec.rb
+++ spec/association_spec.rb
@@ -7,16 +7,20 @@ module Trello
     let(:organization) { client.find(:organization, '4ee7e59ae582acdec8000291') }
     let(:client) { Client.new(consumer_key: 'xxx') }
 
-    before(:each) do
-      client.stub(:get).with('/organizations/4ee7e59ae582acdec8000291', {}).
-        and_return organization_payload
-      client.stub(:get).with('/organizations/4ee7e59ae582acdec8000291/boards/all').
-        and_return boards_payload
+    before do
+      allow(client)
+        .to receive(:get)
+        .with('/organizations/4ee7e59ae582acdec8000291', {})
+        .and_return organization_payload
+
+      allow(client)
+        .to receive(:get)
+        .with('/organizations/4ee7e59ae582acdec8000291/boards/all')
+        .and_return boards_payload
     end
 
     it 'should set the proper client for all associated boards of the organization' do
-      organization.boards.first.client.consumer_key.should == 'xxx'
+      expect(organization.boards.first.client.consumer_key).to eq 'xxx'
     end
-
   end
 end
