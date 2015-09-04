7763502738c3c08ce075c73c18ee91c7d8fbda9a
Adam Cuppy <adam@codingzeal.com>
2015-09-04 14:59:27 -0700

    Migrate spec/hash_spec.rb to `expect` syntax and add a missing assertion (ie: was not testing anything)

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -5,7 +5,11 @@ describe Hash, '#jsoned_into' do
   include Helpers
 
   it "should convert a single parsed json into card" do
-    Trello::Card.should_receive(:new).once.with(cards_details.first)
+    expect(Trello::Card)
+      .to receive(:new)
+      .once
+      .with(cards_details.first)
+
     cards_details.first.jsoned_into(Trello::Card)
   end
 end
