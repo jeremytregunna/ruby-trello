aab67db9dacbf3d89e5b7e8d519b987a3a6572fa
Adam Cuppy <adam@codingzeal.com>
2015-09-21 09:08:29 -0700

    Migrate spec/list_spec.rb to `expect` syntax

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -8,20 +8,30 @@ module Trello
     let(:client) { Client.new }
 
     before(:each) do
-      client.stub(:get).with('/lists/abcdef123456789123456789', {}).and_return JSON.generate(lists_details.first)
-      client.stub(:get).with('/boards/abcdef123456789123456789', {}).and_return JSON.generate(boards_details.first)
+      allow(client)
+        .to receive(:get)
+        .with('/lists/abcdef123456789123456789', {})
+        .and_return JSON.generate(lists_details.first)
+
+      allow(client)
+        .to receive(:get)
+        .with('/boards/abcdef123456789123456789', {})
+        .and_return JSON.generate(boards_details.first)
     end
 
     context 'finding' do
       let(:client) { Trello.client }
 
       it 'delegates to client#find' do
-        client.should_receive(:find).with(:list, 'abcdef123456789123456789', {})
+        expect(client)
+          .to receive(:find)
+          .with(:list, 'abcdef123456789123456789', {})
+
         List.find('abcdef123456789123456789')
       end
 
       it 'is equivalent to client#find' do
-        List.find('abcdef123456789123456789').should eq(list)
+        expect(List.find('abcdef123456789123456789')).to eq(list)
       end
     end
 
@@ -30,17 +40,17 @@ module Trello
 
       it 'creates a new record' do
         list = List.new(lists_details.first)
-        list.should be_valid
+        expect(list).to be_valid
       end
 
       it 'must not be valid if not given a name' do
         list = List.new(lists_details.first.except('name'))
-        list.should_not be_valid
+        expect(list).to_not be_valid
       end
 
       it 'must not be valid if not given a list id' do
         list = List.new(lists_details.first.except('id'))
-        list.should_not be_valid
+        expect(list).to_not be_valid
       end
 
       it 'creates a new record and saves it on Trello', refactor: true do
@@ -54,11 +64,12 @@ module Trello
 
         expected_payload = {name: 'Test List', closed: false, idBoard: 'abcdef123456789123456789', pos: 42}
 
-        client.should_receive(:post).with('/lists', expected_payload).and_return result
+        expect(client)
+          .to receive(:post)
+          .with('/lists', expected_payload).and_return result
 
         list = List.create(payload)
-
-        list.class.should be List
+        expect(list).to be_a List
       end
     end
 
@@ -72,7 +83,11 @@ module Trello
           pos: list.pos
         }
 
-        client.should_receive(:put).once.with('/lists/abcdef123456789123456789', payload)
+        expect(client)
+          .to receive(:put)
+          .once
+          .with('/lists/abcdef123456789123456789', payload)
+
         list.name = expected_new_name
         list.save
       end
@@ -85,7 +100,11 @@ module Trello
           pos: new_position
         }
 
-        client.should_receive(:put).once.with('/lists/abcdef123456789123456789', payload)
+        expect(client)
+          .to receive(:put)
+          .once
+          .with('/lists/abcdef123456789123456789', payload)
+
         list.pos = new_position
         list.save
       end
@@ -93,67 +112,82 @@ module Trello
 
     context 'fields' do
       it 'gets its id' do
-        list.id.should == lists_details.first['id']
+        expect(list.id).to eq lists_details.first['id']
       end
 
       it 'gets its name' do
-        list.name.should == lists_details.first['name']
+        expect(list.name).to eq lists_details.first['name']
       end
 
       it 'knows if it is open or closed' do
-        list.closed.should == lists_details.first['closed']
+        expect(list.closed).to eq lists_details.first['closed']
       end
 
       it 'has a board' do
-        list.board.should == Board.new(boards_details.first)
+        expect(list.board).to eq Board.new(boards_details.first)
       end
 
       it 'gets its position' do
-        list.pos.should == lists_details.first['pos']
+        expect(list.pos).to eq lists_details.first['pos']
       end
     end
 
     context 'actions' do
       it 'has a list of actions' do
-        client.stub(:get).with('/lists/abcdef123456789123456789/actions', { filter: :all }).and_return actions_payload
-        list.actions.count.should be > 0
+        allow(client)
+          .to receive(:get)
+          .with('/lists/abcdef123456789123456789/actions', { filter: :all })
+          .and_return actions_payload
+
+        expect(list.actions.count).to be > 0
       end
     end
 
     context 'cards' do
       it 'has a list of cards' do
-        client.stub(:get).with('/lists/abcdef123456789123456789/cards', { filter: :open }).and_return cards_payload
-        list.cards.count.should be > 0
+        allow(client)
+          .to receive(:get)
+          .with('/lists/abcdef123456789123456789/cards', { filter: :open })
+          .and_return cards_payload
+
+        expect(list.cards.count).to be > 0
       end
 
       it 'moves cards to another list' do
         other_list = List.new(lists_details.first.merge(id: 'otherListID', cards: []))
 
-        client.stub(:post).with('/lists/abcdef123456789123456789/moveAllCards', { idBoard: other_list.board_id, idList: other_list.id }).and_return cards_payload
-        list.move_all_cards(other_list).should eq cards_payload
+        allow(client)
+          .to receive(:post)
+          .with('/lists/abcdef123456789123456789/moveAllCards', { idBoard: other_list.board_id, idList: other_list.id })
+          .and_return cards_payload
+
+        expect(list.move_all_cards(other_list)).to eq cards_payload
       end
     end
 
     describe '#closed?' do
       it 'returns the closed attribute' do
-        expect(list.closed?).not_to be(true)
+        expect(list).to_not be_closed
       end
     end
 
     describe '#close' do
       it 'updates the close attribute to true' do
         list.close
-        expect(list.closed).to be(true)
+        expect(list).to be_closed
       end
     end
 
     describe '#close!' do
       it 'updates the close attribute to true and saves the list' do
-        client.should_receive(:put).once.with('/lists/abcdef123456789123456789', {
-          name: list.name,
-          closed: true,
-          pos: list.pos
-        })
+        expect(client)
+          .to receive(:put)
+          .once
+          .with('/lists/abcdef123456789123456789', {
+            name: list.name,
+            closed: true,
+            pos: list.pos
+          })
 
         list.close!
       end
