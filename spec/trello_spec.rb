aa77a798fe8bb821e24cf1eca6d326333436b525
Adam Cuppy <adam@codingzeal.com>
2015-09-21 08:58:51 -0700

    Migrate spec/trello_spec.rb to `expect` syntax

--- spec/trello_spec.rb
+++ spec/trello_spec.rb
@@ -6,27 +6,24 @@ include Trello::Authorization
 describe Trello do
 
   describe 'self.configure' do
-    it 'builds auth policy client uses to make requests' do
+    before do
       Trello.configure do |config|
         config.developer_public_key = 'developer_public_key'
         config.member_token         = 'member_token'
       end
+    end
 
+    it 'builds auth policy client uses to make requests' do
       TInternet.stub(:execute)
-      Trello.auth_policy.should_receive(:authorize)
+      expect(Trello.auth_policy).to receive(:authorize)
       Trello.client.get(:member, params = {})
     end
 
     it 'configures basic auth policy' do
-      Trello.configure do |config|
-        config.developer_public_key = 'developer_public_key'
-        config.member_token         = 'member_token'
-      end
-
       auth_policy = Trello.auth_policy
-      auth_policy.should be_a(BasicAuthPolicy)
-      auth_policy.developer_public_key.should eq('developer_public_key')
-      auth_policy.member_token.should eq('member_token')
+      expect(auth_policy).to be_a(BasicAuthPolicy)
+      expect(auth_policy.developer_public_key).to eq('developer_public_key')
+      expect(auth_policy.member_token).to eq('member_token')
     end
 
     context 'oauth' do
@@ -42,16 +39,16 @@ describe Trello do
       it 'configures oauth policy' do
         auth_policy = Trello.auth_policy
 
-        auth_policy.should be_a(OAuthPolicy)
-        auth_policy.consumer_key.should eq('consumer_key')
-        auth_policy.consumer_secret.should eq('consumer_secret')
-        auth_policy.oauth_token.should eq('oauth_token')
-        auth_policy.oauth_token_secret.should eq('oauth_token_secret')
+        expect(auth_policy).to be_a(OAuthPolicy)
+        expect(auth_policy.consumer_key).to eq('consumer_key')
+        expect(auth_policy.consumer_secret).to eq('consumer_secret')
+        expect(auth_policy.oauth_token).to eq('oauth_token')
+        expect(auth_policy.oauth_token_secret).to eq('oauth_token_secret')
       end
 
       it 'updates auth policy configuration' do
         auth_policy = Trello.auth_policy
-        auth_policy.consumer_key.should eq('consumer_key')
+        expect(auth_policy.consumer_key).to eq('consumer_key')
 
         Trello.configure do |config|
           config.consumer_key     = 'new_consumer_key'
@@ -62,11 +59,11 @@ describe Trello do
 
         auth_policy = Trello.auth_policy
 
-        auth_policy.should be_a(OAuthPolicy)
-        auth_policy.consumer_key.should eq('new_consumer_key')
-        auth_policy.consumer_secret.should eq('new_consumer_secret')
-        auth_policy.oauth_token.should eq('new_oauth_token')
-        auth_policy.oauth_token_secret.should be_nil
+        expect(auth_policy).to be_a(OAuthPolicy)
+        expect(auth_policy.consumer_key).to eq('new_consumer_key')
+        expect(auth_policy.consumer_secret).to eq('new_consumer_secret')
+        expect(auth_policy.oauth_token).to eq('new_oauth_token')
+        expect(auth_policy.oauth_token_secret).to be_nil
       end
     end
 
@@ -75,7 +72,7 @@ describe Trello do
         Trello.configure
       end
 
-      it { Trello.auth_policy.should be_a(AuthPolicy) }
+      it { expect(Trello.auth_policy).to be_a(AuthPolicy) }
       it { expect { Trello.client.get(:member) }.to raise_error(Trello::ConfigurationError) }
     end
 
