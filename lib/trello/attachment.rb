ebe507fe482c90630f1bf113bd87402075a877e4
Gerrit Riessen <gerrit.riessen@gmail.com>
2015-09-28 12:09:16 +0200

    add previews to the attachment object

--- lib/trello/attachment.rb
+++ lib/trello/attachment.rb
@@ -16,20 +16,21 @@ module Trello
   # @!attribute mime_type
   #   @return [String]
   class Attachment < BasicData
-    register_attributes :name, :id, :url, :bytes, :member_id, :date, :is_upload, :mime_type
+    register_attributes :name, :id, :url, :bytes, :member_id, :date, :is_upload, :mime_type, :previews
     # Update the fields of an attachment.
     #
     # Supply a hash of stringkeyed data retrieved from the Trello API representing
     # an attachment.
     def update_fields(fields)
-      attributes[:name]  = fields['name']
-      attributes[:id] = fields['id']
-      attributes[:url] = fields['url']
-      attributes[:bytes] = fields['bytes'].to_i
+      attributes[:name]      = fields['name']
+      attributes[:id]        = fields['id']
+      attributes[:url]       = fields['url']
+      attributes[:bytes]     = fields['bytes'].to_i
       attributes[:member_id] = fields['idMember']
-      attributes[:date] = Time.parse(fields['date'])
+      attributes[:date]      = Time.parse(fields['date'])
       attributes[:is_upload] = fields['isUpload']
       attributes[:mime_type] = fields['mimeType']
+      attributes[:previews]  = fields['previews']
       self
     end
   end
