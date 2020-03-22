f9c36ce2b20cb0da9c776c6a7294a28f91021860
Hopper Gee <jx4554@gmail.com>
2020-03-22 21:27:20 +0800

    Wrap define_method in class_eval block

--- lib/trello/association_builder/has_many.rb
+++ lib/trello/association_builder/has_many.rb
@@ -3,10 +3,12 @@ module Trello
     class HasMany
       class << self
         def build(model_klass, name, options)
-          model_klass.define_method(name) do |*args|
-            has_many_fetcher = AssociationFetcher::HasMany.new(self, name, options)
-            filter_params = args[0] || {}
-            has_many_fetcher.fetch(filter_params)
+          model_klass.class_eval do
+            define_method(name) do |*args|
+              has_many_fetcher = AssociationFetcher::HasMany.new(self, name, options)
+              filter_params = args[0] || {}
+              has_many_fetcher.fetch(filter_params)
+            end
           end
         end
       end
