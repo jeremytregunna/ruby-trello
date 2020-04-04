8350088bbb006ee77d84c9ba18acdb325e66206a
Hopper Gee <jx4554@gmail.com>
2020-04-04 22:21:06 +0800

    Refactor Trello::RegisterAttributes

--- lib/trello/register_attributes.rb
+++ lib/trello/register_attributes.rb
@@ -5,23 +5,48 @@ module Trello
         names ||= []
         readonly_names ||= []
 
+        define_method_attributes(model_klass, names)
+        define_getters(model_klass, names)
+        define_setters(model_klass, names, readonly_names)
+        track_attributes_changes(model_klass, names)
+      end
+
+      private
+
+      def define_method_attributes(model_klass, names)
         model_klass.class_eval do
           define_method :attributes do
-            @__attributes ||= names.reduce({}) { |hash, k| hash.merge(k.to_sym => nil) }
-          end
-
-          names.each do |key|
-            define_method(:"#{key}") { @__attributes[key] }
-
-            unless readonly_names.include?(key.to_sym)
-              define_method :"#{key}=" do |val|
-                send(:"#{key}_will_change!") unless val == @__attributes[key]
-                @__attributes[key] = val
-              end
+            @__attributes ||= names.reduce({}) do |attrs, name|
+              attrs.merge(name.to_sym => nil)
             end
           end
+        end
+      end
 
-          define_attribute_methods names
+      def define_getters(model_klass, names)
+        model_klass.class_eval do
+          names.each do |key|
+            define_method(key) { @__attributes[key] }
+          end
+        end
+      end
+
+      def define_setters(model_klass, names, readonly_names)
+        model_klass.class_eval do
+          names.each do |key|
+            next if readonly_names.include?(key.to_sym)
+
+            define_method("#{key}=") do |value|
+              send("#{key}_will_change!") if value != @__attributes[key]
+              @__attributes[key] = value
+            end
+          end
+        end
+      end
+
+      def track_attributes_changes(model_klass, names)
+        model_klass.class_eval do
+          define_attribute_methods(names)
         end
       end
     end
