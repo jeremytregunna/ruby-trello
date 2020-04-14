b5aabc4efe3fc387184d6f9023e60dca1d229bbb
Hopper Gee <jx4554@gmail.com>
2020-04-14 21:21:59 +0800

    Add unit test to Trello::Card#closed?

--- spec/unit/trello/card_spec.rb
+++ spec/unit/trello/card_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Trello::Card do
     specify { expect(Trello::Card.path_name).to eq('card') }
   end
 
+  describe '.closed?' do
+    specify { expect(Trello::Card.new('closed' => true).closed?).to eq(true) }
+    specify { expect(Trello::Card.new('closed' => false).closed?).to eq(false) }
+  end
+
 end
