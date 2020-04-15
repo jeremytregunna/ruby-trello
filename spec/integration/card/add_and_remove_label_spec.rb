9348721add4000658c4c3fe67f26187d2f223418
Hopper Gee <jx4554@gmail.com>
2020-04-15 21:02:20 +0800

    Add integration test for Trello::Card#remove_label

--- spec/integration/card/add_and_remove_label_spec.rb
+++ spec/integration/card/add_and_remove_label_spec.rb
@@ -13,8 +13,22 @@ RSpec.describe 'Trell::Card add and remove label' do
         label.name = 'label name' # For past name valiation
 
         response = card.add_label(label)
+        expect(response.code).to eq(200)
         body = JSON.parse(response.body)
         expect(body.first).to eq('5e94f5de7669b2254978bd14')
+      end
+    end
+  end
+
+  describe '#remove_label' do
+    it 'can success remove a label on a card' do
+      VCR.use_cassette('can_remove_label_on_a_card') do
+        card = Trello::Card.find('5e95d1b4f43f9a06497f17f7')
+        label = Trello::Label.find('5e94f5de7669b2254978bd14')
+        label.name = 'label name' # For past name valiation
+
+        response = card.remove_label(label)
+        expect(response.code).to eq(200)
       end
     end
   end
