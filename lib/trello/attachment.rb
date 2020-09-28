76f7346db93057b1ec6a86d9bc8e904de5928b91
Hopper Gee <hopper.gee@hey.com>
2020-09-28 23:05:04 +0800

    Apply it to Attachment and CoverImage

--- lib/trello/attachment.rb
+++ lib/trello/attachment.rb
@@ -17,24 +17,25 @@ module Trello
   #   @return [Boolean]
   # @!attribute mime_type
   #   @return [String]
-  class Attachment < BasicData
-    register_attributes :name, :id, :pos, :url, :bytes, :member_id, :date, :is_upload, :mime_type, :previews
-    # Update the fields of an attachment.
-    #
-    # Supply a hash of stringkeyed data retrieved from the Trello API representing
-    # an attachment.
-    def update_fields(fields)
-      attributes[:name]      = fields['name'] || attributes[:name]
-      attributes[:id]        = fields['id'] || attributes[:id]
-      attributes[:pos]       = fields['pos'] || attributes[:pos]
-      attributes[:url]       = fields['url'] || attributes[:url]
-      attributes[:bytes]     = fields['bytes'].to_i || attributes[:bytes]
-      attributes[:member_id] = fields['idMember'] || attributes[:member_id]
-      attributes[:date]      = Time.parse(fields['date']).presence || attributes[:date]
-      attributes[:is_upload] = fields['isUpload'] if fields.has_key?('isUpload')
-      attributes[:mime_type] = fields['mimeType'] || attributes[:mime_type]
-      attributes[:previews]  = fields['previews'] if fields.has_key?('previews')
-      self
+  class Attachment < BasicDataAlpha
+    schema do
+      # Readonly
+      attribute :id, readonly: true, primary_key: true
+      attribute :position, readonly: true, remote_key: 'pos'
+      attribute :bytes, readonly: true
+      attribute :member_id, remote_key: 'idMember', readonly: true
+      attribute :date, serializer: 'Time', readonly: true
+      attribute :is_upload, remote_key: 'isUpload', readonly: true
+      attribute :previews, readonly: true
+      attribute :file_name, remote_key: 'fileName', readonly: true
+      attribute :edge_color, remote_key: 'edgeColor', readonly: true
+
+      # Writable but create only
+      attribute :name, create_only: true
+      attribute :url, create_only: true
+      attribute :mime_type, remote_key: 'mimeType', create_only: true
+      attribute :file, create_only: true
+      attribute :set_as_cover, remote_key: 'setCover', create_only: true
     end
   end
 end
--- lib/trello/cover_image.rb
+++ lib/trello/cover_image.rb
@@ -4,5 +4,25 @@ module Trello
   # This is normally an attachment that the user (or trello) has set
   # as the cover image
   class CoverImage < Attachment
+    # Same with Attachment may need refactor it to support inheritance
+    schema do
+      # Readonly
+      attribute :id, readonly: true, primary_key: true
+      attribute :position, readonly: true, remote_key: 'pos'
+      attribute :bytes, readonly: true
+      attribute :member_id, remote_key: 'idMember', readonly: true
+      attribute :date, serializer: 'Time', readonly: true
+      attribute :is_upload, remote_key: 'isUpload', readonly: true
+      attribute :previews, readonly: true
+      attribute :file_name, remote_key: 'fileName', readonly: true
+      attribute :edge_color, remote_key: 'edgeColor', readonly: true
+
+      # Writable but create only
+      attribute :name, create_only: true
+      attribute :url, create_only: true
+      attribute :mime_type, remote_key: 'mimeType', create_only: true
+      attribute :file, create_only: true
+      attribute :set_as_cover, remote_key: 'setCover', create_only: true
+    end
   end
 end
